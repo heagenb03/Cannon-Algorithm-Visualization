cff59f4e3b6fc3cdfb323e1b8265700d8c10c03f
heagenb03 <heagenbell@outlook.com>
2025-01-10 16:25:22 -0600

    added scene 1, scene 2, start on scene 3

--- main.py
+++ main.py
@@ -1,9 +1,98 @@
-from manim import Scene, VGroup, MathTex, FadeIn, FadeOut
+from manim import Scene, VGroup, MathTex, FadeIn, FadeOut, LEFT, RIGHT, ORIGIN
 import sys
 sys.path.insert(0, 'scenes')
 
 from constants import *
+from scenes.scene1 import Scene1
+from scenes.scene2 import Scene2
+from scenes.scene3 import Scene3
 
-class Snyder(Scene):
+class Cannon(Scene):
     def construct(self):
-        pass
+        scene1 = Scene1()
+        scene2 = Scene2()
+        scene3 = Scene3()
+        
+        """
+        Scene 1 
+            #1. Create Matrix A, B, C
+            #2. Move Aij & Bij values to corresponding Cij values / Fadeout Matrix A & B
+        """
+        
+        #1        
+        multi_sign = MathTex("\\times")
+        equal_sign = MathTex("=")
+        
+        matrixA_scene1 = scene1.createMatrixA()
+        matrixB_scene1 = scene1.createMatrixB()
+        matrixC_scene1 = scene1.createMatrixC()
+        algorithm_title = scene1.createTitle()
+        
+        matrices = VGroup(
+            matrixC_scene1,
+            equal_sign,
+            matrixB_scene1,
+            multi_sign,
+            matrixA_scene1
+        ).arrange(LEFT, buff=MATRIX_BUFFER*2)
+        
+        matrices.move_to(ORIGIN)
+        
+        self.add(algorithm_title)
+        self.play(FadeIn(matrices))
+        self.wait(5)
+        
+        #2
+        move_animations = scene1.moveEnteriesToMatrixC(matrixA_scene1, matrixB_scene1, matrixC_scene1)
+        self.play(*move_animations)
+        self.wait(0.5)
+        
+        partial_matrixC_scene = scene1.createPartialMatrixC()
+        
+        self.play(FadeOut(matrices),
+                FadeIn(partial_matrixC_scene.shift(RIGHT * scene1.RIGHT_ALINGMENT))
+        )
+        self.wait(0.5)
+        
+        """
+        Scene 2 
+            1. Move Matrix C to center
+            2. Realign Matrix C for future animation purposes
+        """
+        
+        #1
+        matrixC_scene2 = scene2.createMatrixC()
+
+
+        move_animations = scene2.moveMatrixCtoCenter(matrixB_scene1, partial_matrixC_scene)
+        self.play(*move_animations)
+        self.play(FadeOut(partial_matrixC_scene),
+                FadeIn(matrixC_scene2.shift(RIGHT * scene2.RIGHT_ALINGMENT))
+        )
+        
+        #2
+        adjust_animations = scene2.realignMatrixC(matrixC_scene2)
+        self.play(*adjust_animations)
+        
+        self.wait(1)
+        
+        """
+        Scene 3
+            1. Move correspodning Aij values across the matrix
+            2. Move correspodning Bij values across the matrix
+        """
+        #1
+        for row in range(1, MATRIX_ROW_COL_CT):
+            for count in range(row):
+                move_animations = scene3.moveAValuesAcross(matrixC_scene2, row)
+                self.play(*move_animations)
+                self.wait(0.25)
+        
+        #2
+        for col in range(1, MATRIX_ROW_COL_CT):
+            for count in range(col):
+                move_animations = scene3.moveBValuesUp(matrixC_scene2, col)
+                self.play(*move_animations)
+                self.wait(0.25)
+        
+        self.wait(1)
