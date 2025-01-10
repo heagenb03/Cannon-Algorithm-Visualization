c4144ed9595ec037c1aa1298f90a6e9486f6e6e3
heagenb03 <heagenbell@outlook.com>
2025-01-10 15:18:30 -0600

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from manim import Scene, VGroup, MathTex, FadeIn, FadeOut
+import sys
+sys.path.insert(0, 'scenes')
+
+from constants import *
+
+class Snyder(Scene):
+    def construct(self):
+        pass
